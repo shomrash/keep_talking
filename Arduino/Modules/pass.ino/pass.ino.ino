3cb3a0dd723ba8f291d6e69458fb80305e14afdb
shomrash <shomrash@live.ru>
2020-02-21 22:22:59 +0300

    Create pass.ino.ino

---
+++ Arduino/Modules/pass.ino/pass.ino.ino
@@ -0,0 +1,26 @@
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  u8g.setPrintPos(0, 20); 
+  // call procedure from base class, http://arduino.cc/en/Serial/Print
+  u8g.print("Hello World!");
+}
+
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
+}
+
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  delay(500);
+}
